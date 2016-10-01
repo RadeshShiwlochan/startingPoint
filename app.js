c93bf729a10d6cb17b9c2b44d803619eeb926718
RadeshShiwlochan <radesh0430@gmail.com>
2016-09-30 23:23:50 -0400

    adds connection to MongoDB

--- app.js
+++ app.js
@@ -3,12 +3,24 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path'),
+    MongoClient = require('mongodb').MongoClient,
+    db,
     app = express();
+
+const connectString = 'mongodb://localhost:27017';
+
 
 //app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+MongoClient.connect(connectString, (err, dataBase) => {
+	if(err) 
+		throw err;
+	console.log("Successfully connected to the database");
+	db = dataBase;
+});
 
 app.get('/', (req, res, next) => {
 	res.render('index');
