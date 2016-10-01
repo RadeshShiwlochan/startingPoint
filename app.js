e5ab89a155c1cec2d1ecd25cdb4157222d6eb7e6
RadeshShiwlochan <radesh0430@gmail.com>
2016-09-30 23:00:19 -0400

    adds a public folder with stylesheet

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require('express'),
 //app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
 	res.render('index');
