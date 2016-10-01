b3debc1ebca10430ed7dc15ec97209f021f7b9f0
RadeshShiwlochan <radesh0430@gmail.com>
2016-09-30 22:48:04 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    app = express();
+
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res, next) => {
+	res.render('index');
+});
+
+app.listen(3000, () =>{
+	console.log('listening on Port:3000');
+});    
+
